20ea7f21f8db276672d5dc570743e513470ae98f
Mohammad Javad Naderi <mjnaderi@gmail.com>
2021-04-23 18:44:31 +0430

    Release version 0.2.0
    Make it work in Mattermost 5.34
    Update npm packages
    Update Vazir font

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,7 +4,11 @@ import "./style.css"
 class MattermostRTLPlugin {
     initialize(registry, store) {
         console.info("Hi! Mattermost RTL plugin initialized...");
-        setInterval(this.autoDirection, 5000);
+        this.interval = setInterval(this.autoDirection, 5000);
+    }
+
+    uninitialize() {
+        clearInterval(this.interval);
     }
 
     autoDirection() {
@@ -12,6 +16,18 @@ class MattermostRTLPlugin {
             ".post-message__text > p, .post-message__text > ul, .post-message__text > ol";
         for(const element of document.querySelectorAll(selector)) {
             element.setAttribute("dir", "auto")
+        }
+
+        /**
+         *  In Mattermost 5.34, this CSS rule exists:
+         *
+         *      .post-message__text > ul { direction: ltr }
+         *
+         *  We need to cancel its effect. Overriding CSS does not work. We wrap the <ul> element in a <div>,
+         *  so the CSS selector does not match it anymore.
+         */
+        for(const element of document.querySelectorAll(".post-message__text > ul")) {
+            element.outerHTML = "<div>" + element.outerHTML + "</div>";
         }
     }
 }
