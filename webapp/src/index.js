41eb082be0aa6a6df21ed929e10b5ec2052900ef
Mohammad Javad Naderi <mjnaderi@gmail.com>
2023-07-27 13:45:17 +0330

    Update dependencies and add "standard" code style

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,35 +1,35 @@
-import manifest from "./manifest";
-import "./style.css"
+import manifest from './manifest'
+import './style.css'
 
 class MattermostRTLPlugin {
-    initialize(registry, store) {
-        console.info("Hi! Mattermost RTL plugin initialized...");
-        this.interval = setInterval(this.autoDirection, 5000);
+  initialize (registry, store) {
+    console.info('Hi! Mattermost RTL plugin initialized...')
+    this.interval = setInterval(this.autoDirection, 5000)
+  }
+
+  uninitialize () {
+    clearInterval(this.interval)
+  }
+
+  autoDirection () {
+    const selector = '#post_textbox, #edit_textbox, #reply_textbox, #searchBox, ' +
+            '.post-message__text > p, .post-message__text > ul, .post-message__text > ol'
+    for (const element of document.querySelectorAll(selector)) {
+      element.setAttribute('dir', 'auto')
     }
 
-    uninitialize() {
-        clearInterval(this.interval);
+    /**
+     *  In Mattermost 5.34, this CSS rule exists:
+     *
+     *      .post-message__text > ul { direction: ltr }
+     *
+     *  We need to cancel its effect. Overriding CSS does not work. We wrap the <ul> element in a <div>,
+     *  so the CSS selector does not match it anymore.
+     */
+    for (const element of document.querySelectorAll('.post-message__text > ul')) {
+      element.outerHTML = '<div>' + element.outerHTML + '</div>'
     }
-
-    autoDirection() {
-        const selector = "#post_textbox, #edit_textbox, #reply_textbox, #searchBox, " +
-            ".post-message__text > p, .post-message__text > ul, .post-message__text > ol";
-        for(const element of document.querySelectorAll(selector)) {
-            element.setAttribute("dir", "auto")
-        }
-
-        /**
-         *  In Mattermost 5.34, this CSS rule exists:
-         *
-         *      .post-message__text > ul { direction: ltr }
-         *
-         *  We need to cancel its effect. Overriding CSS does not work. We wrap the <ul> element in a <div>,
-         *  so the CSS selector does not match it anymore.
-         */
-        for(const element of document.querySelectorAll(".post-message__text > ul")) {
-            element.outerHTML = "<div>" + element.outerHTML + "</div>";
-        }
-    }
+  }
 }
 
-window.registerPlugin(manifest.id, new MattermostRTLPlugin());
+window.registerPlugin(manifest.id, new MattermostRTLPlugin())
--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,43 +1,43 @@
-var path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: [
-        './src/index.js',
+  entry: [
+    './src/index.js'
+  ],
+  resolve: {
+    modules: [
+      'src',
+      'node_modules'
     ],
-    resolve: {
-        modules: [
-            'src',
-            'node_modules',
-        ],
-        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true,
-                        // Babel configuration is in babel.config.js because jest requires it to be there.
-                    },
-                },
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: {
-                        url: false,
-                    },
-                }],
-            }
-        ],
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        publicPath: '/',
-        filename: 'main.js',
-    },
-};
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+            // Babel configuration is in babel.config.js because jest requires it to be there.
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            url: false
+          }
+        }]
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'main.js'
+  }
+}
