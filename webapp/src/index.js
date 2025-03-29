a3040e1d84ec19b48bca897e15a1b6d116a99783
Mohammad Javad Naderi <mjnaderi@gmail.com>
2025-03-29 18:08:47 +0330

    Update dependencies

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,19 +1,19 @@
-import manifest from './manifest';
-import './style.scss';
+import manifest from "./manifest";
+import "./style.scss";
 
 function fixMattermostDirections() {
   const selector = [
-    '#post_textbox',
-    '#edit_textbox',
-    '#reply_textbox',
-    '#searchBox',
-    '.post-message__text > p',
-    '.post-message__text > ul',
-    '.post-message__text > ol',
-    '#quickSwitchInput',
-  ].join(', ');
+    "#post_textbox",
+    "#edit_textbox",
+    "#reply_textbox",
+    "#searchBox",
+    ".post-message__text > p",
+    ".post-message__text > ul",
+    ".post-message__text > ol",
+    "#quickSwitchInput",
+  ].join(", ");
   document.querySelectorAll(selector).forEach((element) => {
-    element.setAttribute('dir', 'auto');
+    element.setAttribute("dir", "auto");
   });
 
   // In Mattermost 5.34, this CSS rule exists:
@@ -22,28 +22,32 @@ function fixMattermostDirections() {
   //
   // We need to cancel its effect. Overriding CSS does not work.
   // We wrap the <ul> element in a <div>, so the CSS selector does not match it anymore.
-  document.querySelectorAll('.post-message__text > ul').forEach((element) => {
-    // eslint-disable-next-line no-param-reassign
+  document.querySelectorAll(".post-message__text > ul").forEach((element) => {
     element.outerHTML = `<div>${element.outerHTML}</div>`;
   });
 }
 
 function fixBoardsDirections() {
   const selector = [
-    '.focalboard-body .Editable',
-    '.focalboard-body input',
-    '.focalboard-body .MarkdownEditor',
+    ".focalboard-body .Editable",
+    ".focalboard-body input",
+    ".focalboard-body .MarkdownEditor",
     '.focalboard-body [data-block="true"][data-editor]',
-    '.focalboard-body .BoardsSwitcher input.searchQuery',
-  ].join(', ');
+    ".focalboard-body .BoardsSwitcher input.searchQuery",
+  ].join(", ");
   document.querySelectorAll(selector).forEach((element) => {
-    element.setAttribute('dir', 'auto');
+    element.setAttribute("dir", "auto");
   });
 
   // Fix checkbox element directions
-  document.querySelectorAll('.CheckboxElement input.Editable').forEach((element) => {
-    element.parentElement.setAttribute('dir', getComputedStyle(element).direction);
-  });
+  document
+    .querySelectorAll(".CheckboxElement input.Editable")
+    .forEach((element) => {
+      element.parentElement.setAttribute(
+        "dir",
+        getComputedStyle(element).direction
+      );
+    });
 }
 
 function fixDirections() {
